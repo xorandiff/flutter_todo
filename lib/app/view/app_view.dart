7aa2656463e5a706fcf975af0d63112d28e49292
xorandiff <roxav@protonmail.com>
2024-05-02 17:59:00 +0200

    Add images

--- lib/app/view/app_view.dart
+++ lib/app/view/app_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_todo/app/app.dart';
 import 'package:flutter_todo/app/bloc/app_bloc.dart';
 import 'package:flutter_todo/app/widget/pro_banner.dart';
 import 'package:gap/gap.dart';
@@ -12,15 +13,62 @@ class AppView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color.fromRGBO(243, 243, 243, 1.0),
+      appBar: AppBar(
+        foregroundColor: Colors.white,
+        title: const Row(
+          children: [
+            CircleAvatar(
+              backgroundImage: AssetImage('images/me.png'),
+            ),
+            Gap(16.0),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Hello, Mateusz',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    shadows: [
+                      Shadow(
+                        color: Colors.black,
+                        offset: Offset(0, 2.0),
+                      ),
+                    ],
+                  ),
+                ),
+                Text(
+                  'mateusz.struk@gmail.com',
+                  style: TextStyle(
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.w100,
+                    fontStyle: FontStyle.italic,
+                    shadows: [
+                      Shadow(
+                        color: Colors.black,
+                        offset: Offset(0, 2.0),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+        backgroundColor: const Color.fromRGBO(53, 86, 171, 1.0),
+      ),
       body: Column(
         children: [
           const ProBanner(),
           Expanded(
             child: BlocBuilder<AppBloc, AppState>(
               builder: (context, state) => ListView.separated(
-                itemBuilder: (context, index) => const Gap(12.0),
+                padding: const EdgeInsets.all(16.0),
+                itemBuilder: (context, index) => TaskListItem(
+                  task: state.tasks[index],
+                ),
                 separatorBuilder: (context, index) => const Gap(12.0),
-                itemCount: 2,
+                itemCount: state.tasks.length,
               ),
             ),
           ),
--- lib/app/widget/task_list_item.dart
+++ lib/app/widget/task_list_item.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_todo/app/model/model.dart';
 
 class TaskListItem extends StatelessWidget {
-  const TaskListItem({super.key});
+  final Task task;
+
+  const TaskListItem({
+    super.key,
+    required this.task,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -18,27 +24,49 @@ class TaskListItem extends StatelessWidget {
           decoration: BoxDecoration(
             shape: BoxShape.circle,
             border: Border.all(
-              color: const Color.fromRGBO(7, 29, 85, 1.0),
+              color: Colors.transparent,
             ),
           ),
           child: Container(
             decoration: BoxDecoration(
               shape: BoxShape.circle,
               border: Border.all(
-                color: const Color.fromRGBO(73, 194, 93, 1.0),
+                color: task.isDone
+                    ? const Color.fromRGBO(73, 194, 93, 1.0)
+                    : const Color.fromRGBO(7, 29, 85, 1.0),
+                width: 2.0,
                 style: BorderStyle.solid,
               ),
-              color: const Color.fromRGBO(83, 218, 105, 1.0),
+              color:
+                  task.isDone ? const Color.fromRGBO(83, 218, 105, 1.0) : null,
             ),
             padding: const EdgeInsets.all(4.0),
-            child: const Icon(
-              Icons.check_rounded,
-              size: 18.0,
-              color: Color.fromRGBO(57, 150, 73, 1.0),
-            ),
+            child: task.isDone
+                ? const Icon(
+                    Icons.check_rounded,
+                    size: 18.0,
+                    color: Color.fromRGBO(57, 150, 73, 1.0),
+                  )
+                : const SizedBox(
+                    width: 18.0,
+                    height: 18.0,
+                  ),
           ),
         ),
-        title: const Text('Task'),
+        title: Text(
+          task.name,
+          style: task.isDone
+              ? const TextStyle(
+                  color: Color.fromRGBO(141, 141, 141, 1.0),
+                  fontWeight: FontWeight.bold,
+                  decoration: TextDecoration.lineThrough,
+                  decorationColor: Color.fromRGBO(141, 141, 141, 1.0),
+                )
+              : const TextStyle(
+                  color: Color.fromRGBO(7, 29, 85, 1.0),
+                  fontWeight: FontWeight.bold,
+                ),
+        ),
         trailing: OutlinedButton(
           onPressed: () {},
           style: OutlinedButton.styleFrom(
